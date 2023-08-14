131745aad3530a379b32ebb5cc40cd0468ad5bcd
20pa1a5708 <113027359+20pa1a5708@users.noreply.github.com>
2023-08-14 22:20:41 +0530

    remainder mod11.java

---
+++ remainder mod11.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s=input.next();
+        int n=Integer.parseInt(s);
+        System.out.println(n%11);
+    }
+}
